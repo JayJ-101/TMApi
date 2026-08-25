5df258befb38d55ccb7b399a5b22d8f401198e8e
Sinaye Tontsi <sxolise101@gmail.com>
2026-08-25 18:37:36 +0200

    Add project files.

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TMApi.Data;
+using TMApi.Models;
+
+namespace TMApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class TasksController    : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetTasks()
+        {
+            var tasks = _context.TaskItems.ToList();
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTask(TaskItem task)
+        {
+            task.CreatedAt = DateTime.UtcNow;
+            task.IsCompleted = false;
+
+            _context.TaskItems.Add(task);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetTasks), 
+                new { id = task.Id },
+                task);
+
+        }
+
+        
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMApi.Models;
+
+namespace TMApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
